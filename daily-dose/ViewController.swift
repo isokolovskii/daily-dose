632789d4b7421a45c069c75db788ef562bddffa7
Ivan Sokolovskii <ivan3177@gmail.com>
2018-01-28 17:47:10 +0300

    Created Xcode project to implement in-app purchases

---
+++ daily-dose/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  daily-dose
+//
+//  Created by Иван on 28.01.2018.
+//  Copyright © 2018 iSOKOL DEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
